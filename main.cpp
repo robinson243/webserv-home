66e1b9bd4c3f3b72a16864f9663e50df9dbf2bac
mukena_r <robinsonmukena@gmail.com>
2026-04-20 23:09:32 +0200

    need to parse arg

--- main.cpp
+++ main.cpp
@@ -1,28 +1,22 @@
 #include "HttpRequest.hpp"
 
 #include <sstream>
-int main() {
+int main()
+{
 	HttpRequest test;
-	// std::string str = "POST /upload/fichier.txt HTTP/1.1\r\n"
-	// 				  "Host: localhost:8080\r\n"
-	// 				  "Content-Length: 29\r\n"
-	// 				  "\r\n"
-	// 				  "Bonjour,\n"
-	// 				  "voici mon fichier !";
-	// std::stringstream ss(str);
-	// std::string token;
-	// while (ss >> token) {
-	// 	std::cout << "word " << token << std::endl;
-	// }
-	std::string te, le, re;
-	te = "test";
-	le = "element";
-	re = "element2";
-	test.addRequest(te, le);
-	test.addRequest(te, re);
-	for (size_t i = 0; i < test.getRequest().size(); i++)
+	std::string request =
+		"POST /upload HTTP/1.1\r\n"
+		"Host: localhost:8080\r\n"
+		"Content-Type: text/plain\r\n"
+		"Content-Length: 13\r\n"
+		"Connection: close\r\n"
+		"\r\n"
+		"Hello, world!";
+	std::stringstream str(request);
+	std::string token;
+	while (str >> token)
 	{
-		std::cout << "key: " << test.getRequest()["test"] << std::endl; 
+		std::cout << "word :" << token << std::endl;
 	}
 	
 
