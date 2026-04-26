4b9dfd7c80dfc383ee48f3184bbf4b3d284818b7
mukena_r <robinsonmukena@gmail.com>
2026-04-27 00:04:33 +0200

    finish get

--- RequestHandler.hpp
+++ RequestHandler.hpp
@@ -6,7 +6,7 @@
 /*   By: romukena <romukena@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/24 15:50:44 by romukena          #+#    #+#             */
-/*   Updated: 2026/04/25 19:09:15 by romukena         ###   ########.fr       */
+/*   Updated: 2026/04/27 00:04:31 by romukena         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,5 @@ int findLocation(ServerConfig server, HttpRequest req);
 std::string concatenatePath(ServerConfig server, HttpRequest req);
 bool readFileToString(const std::string &path, std::string &content);
 std::string getContentType(const std::string &path);
-std::string generateAutoindex(const std::string &path, const std::string &uri);
 HttpResponse Get(const HttpRequest &req, const ServerConfig &server);
+HttpResponse Delete(const HttpRequest &req, const ServerConfig &server);
