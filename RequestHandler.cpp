3c3bbe3927a40a86acaada73d57a10e161844c10
mukena_r <robinsonmukena@gmail.com>
2026-04-30 01:46:44 +0200

    need to fix some stuff

--- RequestHandler.cpp
+++ RequestHandler.cpp
@@ -6,7 +6,7 @@
 /*   By: romukena <romukena@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/24 14:36:05 by romukena          #+#    #+#             */
-/*   Updated: 2026/04/28 00:30:31 by romukena         ###   ########.fr       */
+/*   Updated: 2026/04/30 01:36:46 by romukena         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,19 +22,19 @@
 #include <string>
 #include <sys/stat.h>
 
-int findLocation(ServerConfig server, HttpRequest req) {
+int findLocation(ServerConfig server, HttpRequest req)
+{
 	std::map<std::string, std::string> r = req.getRequest();
 	std::string uri = r["uri"];
 	std::vector<LocationConfig>::iterator it;
 	std::vector<LocationConfig> loc = server.getLocations();
 	int val = -1;
 	size_t longestMatch = 0;
-	for (it = loc.begin(); it != loc.end(); ++it) {
+	for (it = loc.begin(); it != loc.end(); ++it)
+	{
 		std::string path = (*it).getPath();
-		if (uri.compare(0, path.size(), path) == 0
-			&& (uri.size() == path.size() || uri[path.size()] == '/'
-				|| path == "/")
-			&& path.size() > longestMatch) {
+		if (uri.compare(0, path.size(), path) == 0 && (uri.size() == path.size() || uri[path.size()] == '/' || path == "/") && path.size() > longestMatch)
+		{
 			longestMatch = path.size();
 			val = std::distance(loc.begin(), it);
 		}
@@ -42,7 +42,8 @@ int findLocation(ServerConfig server, HttpRequest req) {
 	return val;
 }
 
-std::string concatenatePath(ServerConfig server, HttpRequest req) {
+std::string concatenatePath(ServerConfig server, HttpRequest req)
+{
 	std::map<std::string, std::string> r = req.getRequest();
 	std::string uri = r["uri"];
 	if (uri.find("/..") != std::string::npos)
@@ -51,7 +52,8 @@ std::string concatenatePath(ServerConfig server, HttpRequest req) {
 	std::string finalPath = root + uri;
 
 	size_t p = finalPath.find("//");
-	while (p != std::string::npos) {
+	while (p != std::string::npos)
+	{
 		finalPath.erase(p, 1);
 		p = finalPath.find("//");
 	}
@@ -61,8 +63,10 @@ std::string concatenatePath(ServerConfig server, HttpRequest req) {
 		return "";
 	std::string absRoot(resolvedRoot);
 	free(resolvedRoot);
+	finalPath = absRoot + uri;
 	char *resolvedPath = realpath(finalPath.c_str(), NULL);
-	if (!resolvedPath) {
+	if (!resolvedPath)
+	{
 		if (finalPath.find(absRoot) != 0)
 			return "";
 		return finalPath;
@@ -76,7 +80,8 @@ std::string concatenatePath(ServerConfig server, HttpRequest req) {
 	return absPath;
 }
 
-bool readFileToString(const std::string &path, std::string &content) {
+bool readFileToString(const std::string &path, std::string &content)
+{
 	std::ifstream file(path.c_str());
 	if (!file.is_open())
 		return false;
@@ -86,7 +91,8 @@ bool readFileToString(const std::string &path, std::string &content) {
 	return true;
 }
 
-std::string getContentType(const std::string &path) {
+std::string getContentType(const std::string &path)
+{
 	size_t pos = path.find_last_of('.');
 	if (pos == std::string::npos)
 		return "text/plain";
@@ -115,25 +121,31 @@ std::string getContentType(const std::string &path) {
 	return "application/octet-stream";
 }
 
-HttpResponse Get(const HttpRequest &req, const ServerConfig &server) {
+HttpResponse Get(const HttpRequest &req, const ServerConfig &server)
+{
 	struct stat st;
 	HttpResponse response;
 	int valLocation = findLocation(server, req);
 	std::vector<LocationConfig> locations = server.getLocations();
-	if (locations.empty() || valLocation == -1) {
+	if (locations.empty() || valLocation == -1)
+	{
 		response.addCode(404);
 		return response;
 	}
 	std::vector<std::string> indexes = locations[valLocation].getIndex();
 	std::string path = concatenatePath(server, req);
-	if (path.empty()) {
+	if (path.empty())
+	{
 		response.addCode(403);
 		return response;
 	}
-	if (stat(path.c_str(), &st) == 0) {
-		if (S_ISREG(st.st_mode)) {
+	if (stat(path.c_str(), &st) == 0)
+	{
+		if (S_ISREG(st.st_mode))
+		{
 			std::string body;
-			if (!readFileToString(path, body)) {
+			if (!readFileToString(path, body))
+			{
 				response.addCode(403);
 				return response;
 			}
@@ -145,9 +157,12 @@ HttpResponse Get(const HttpRequest &req, const ServerConfig &server) {
 			response.addHeadersResponse("Content-Length", oss.str());
 			response.addBodyResponse(body);
 			return response;
-		} else if (S_ISDIR(st.st_mode)) {
+		}
+		else if (S_ISDIR(st.st_mode))
+		{
 			std::string uri = req.getRequest().at("uri");
-			if (uri.empty() || uri[uri.size() - 1] != '/') {
+			if (uri.empty() || uri[uri.size() - 1] != '/')
+			{
 				response.addCode(301);
 				response.addHeadersResponse("Location", uri + "/");
 				return response;
@@ -155,13 +170,15 @@ HttpResponse Get(const HttpRequest &req, const ServerConfig &server) {
 			// Sous-cas 1 : chercher un fichier index
 			for (std::vector<std::string>::iterator it = indexes.begin();
 				 it != indexes.end();
-				 ++it) {
+				 ++it)
+			{
 				std::string indexPath = path + "/" + *it;
 				struct stat stIndex;
-				if (stat(indexPath.c_str(), &stIndex) == 0
-					&& S_ISREG(stIndex.st_mode)) {
+				if (stat(indexPath.c_str(), &stIndex) == 0 && S_ISREG(stIndex.st_mode))
+				{
 					std::string body;
-					if (!readFileToString(indexPath, body)) {
+					if (!readFileToString(indexPath, body))
+					{
 						response.addCode(403);
 						return response;
 					}
@@ -176,21 +193,23 @@ HttpResponse Get(const HttpRequest &req, const ServerConfig &server) {
 				}
 			}
 
-			if (locations[valLocation].getAutoindex()) {
+			if (locations[valLocation].getAutoindex())
+			{
 				DIR *dir = opendir(path.c_str());
-				if (!dir) {
+				if (!dir)
+				{
 					response.addCode(403);
 					return response;
 				}
 
 				std::string uri = req.getRequest().at("uri");
 				std::string html;
-				html += "<html><head><title>Index of " + uri
-						+ "</title></head><body>";
+				html += "<html><head><title>Index of " + uri + "</title></head><body>";
 				html += "<h1>Index of " + uri + "</h1><ul>";
 
 				struct dirent *entry;
-				while ((entry = readdir(dir)) != NULL) {
+				while ((entry = readdir(dir)) != NULL)
+				{
 					std::string name = entry->d_name;
 					if (name == ".")
 						continue;
@@ -200,8 +219,8 @@ HttpResponse Get(const HttpRequest &req, const ServerConfig &server) {
 					std::string href = name;
 					std::string display = name;
 
-					if (stat(fullPath.c_str(), &stEntry) == 0
-						&& S_ISDIR(stEntry.st_mode)) {
+					if (stat(fullPath.c_str(), &stEntry) == 0 && S_ISDIR(stEntry.st_mode))
+					{
 						href = name + "/";
 						display = name + "/";
 					}
@@ -221,7 +240,9 @@ HttpResponse Get(const HttpRequest &req, const ServerConfig &server) {
 				response.addHeadersResponse("Content-Length", oss.str());
 				response.addBodyResponse(html);
 				return response;
-			} else {
+			}
+			else
+			{
 				response.addCode(403);
 				return response;
 			}
@@ -231,65 +252,78 @@ HttpResponse Get(const HttpRequest &req, const ServerConfig &server) {
 	return response;
 }
 
-HttpResponse Delete(const HttpRequest &req, const ServerConfig &server) {
+HttpResponse Delete(const HttpRequest &req, const ServerConfig &server)
+{
 	struct stat st;
 	HttpResponse response;
 	int valLocation = findLocation(server, req);
 	std::vector<LocationConfig> locations = server.getLocations();
-	if (locations.empty() || valLocation == -1) {
+	if (locations.empty() || valLocation == -1)
+	{
 		response.addCode(404);
 		return response;
 	}
 	std::string path = concatenatePath(server, req);
-	if (path == "") {
+	if (path == "")
+	{
 		response.addCode(403);
 		return response;
 	}
-	if (stat(path.c_str(), &st) == -1) {
+	if (stat(path.c_str(), &st) == -1)
+	{
 		response.addCode(404);
 		return response;
 	}
-	if (S_ISDIR(st.st_mode)) {
+	if (S_ISDIR(st.st_mode))
+	{
 		response.addCode(403);
 		return response;
 	}
-	if (remove(path.c_str()) == 0) {
+	if (remove(path.c_str()) == 0)
+	{
 		response.addCode(204);
 		return response;
-	} else {
+	}
+	else
+	{
 		response.addCode(403);
 		return response;
 	}
 }
 
-HttpResponse Post(const HttpRequest &req, const ServerConfig &server) {
+HttpResponse Post(const HttpRequest &req, const ServerConfig &server)
+{
 	struct stat st;
 	HttpResponse response;
 	int valLocation = findLocation(server, req);
 	std::vector<LocationConfig> locations = server.getLocations();
 	std::vector<unsigned char> body = req.getBody();
 
-	if (locations.empty() || valLocation == -1) {
+	if (locations.empty() || valLocation == -1)
+	{
 		response.addCode(404);
 		return response;
 	}
-	if (!locations[valLocation].isMethodAllowed("POST")) {
+	if (!locations[valLocation].isMethodAllowed("POST"))
+	{
 		response.addCode(405);
 		return response;
 	}
-	if (locations[valLocation].gethasmaxsize()
-		&& locations[valLocation].getMaxBody() < body.size()) {
+	if (locations[valLocation].gethasmaxsize() && locations[valLocation].getMaxBody() < body.size())
+	{
 		response.addCode(413);
 		return response;
 	}
-	if (locations[valLocation].getUploadPath().empty()) {
+	if (locations[valLocation].getUploadPath().empty())
+	{
 		response.addCode(500);
 		return response;
 	}
 
 	std::map<std::string, std::string> r = req.getRequest();
 	std::string uri = r["uri"];
-	if (uri.empty()) {
+	if (uri.empty())
+	{
 		response.addCode(400);
 		return response;
 	}
@@ -299,18 +333,21 @@ HttpResponse Post(const HttpRequest &req, const ServerConfig &server) {
 		URI = uri;
 	else
 		URI = uri.substr(p + 1);
-	if (URI.empty()) {
+	if (URI.empty())
+	{
 		response.addCode(400);
 		return response;
 	}
-	if (URI.find("..") != std::string::npos) {
+	if (URI.find("..") != std::string::npos)
+	{
 		response.addCode(403);
 		return response;
 	}
 	std::string uploadPath = locations[valLocation].getUploadPath();
 	std::string path = uploadPath + "/" + URI;
 	std::ofstream file(path.c_str(), std::ios::binary);
-	if (!file.is_open()) {
+	if (!file.is_open())
+	{
 		response.addCode(403);
 		return response;
 	}
@@ -321,10 +358,12 @@ HttpResponse Post(const HttpRequest &req, const ServerConfig &server) {
 	return response;
 }
 
-HttpResponse handleRequest(const HttpRequest &req, const ServerConfig &server) {
+HttpResponse handleRequest(const HttpRequest &req, const ServerConfig &server)
+{
 	HttpResponse response;
 	int valLocation = findLocation(server, req);
-	if (valLocation == -1) {
+	if (valLocation == -1)
+	{
 		response.addCode(404);
 		return response;
 	}
@@ -334,7 +373,8 @@ HttpResponse handleRequest(const HttpRequest &req, const ServerConfig &server) {
 	std::string uri = r["uri"];
 	std::set<std::string> allowMeth = locations[valLocation].getAllowMethods();
 
-	if (allowMeth.find(r["method"]) == allowMeth.end()) {
+	if (allowMeth.find(r["method"]) == allowMeth.end())
+	{
 		response.addCode(405);
 		return response;
 	}
@@ -345,7 +385,8 @@ HttpResponse handleRequest(const HttpRequest &req, const ServerConfig &server) {
 		response = Post(req, server);
 	else if (r["method"] == "DELETE")
 		response = Delete(req, server);
-	else {
+	else
+	{
 		response.addCode(404);
 		return response;
 	}
--- main.cpp
+++ main.cpp
@@ -1,114 +1,296 @@
 #include "HttpRequest.hpp"
-#include "LocationConfig.hpp"
-#include "ServerConfig.hpp"
-
-// int main() {
-// 	HttpRequest test;
-// 	std::string request = "POST /upload HTTP/1.1\r\n"
-// 						  "Host: localhost:8080\r\n"
-// 						  "Content-Type: text/plain\r\n"
-// 						  "Content-Length: 13\r\n"
-// 						  "Connection: close\r\n"
-// 						  "\r\n"
-// 						  "Hello, world!";
-
-// 	test.addHttpRequest(request);
-// 	test.print();
-
-// 	return 0;
-// }
-#include "HttpRequest.hpp"
 #include "HttpResponse.hpp"
 #include "ServerConfig.hpp"
 #include "LocationConfig.hpp"
 #include "RequestHandler.hpp"
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <sys/stat.h>
+
+// ─────────────────────────────────────────────────────────────────────────────
+// Helpers
+// ─────────────────────────────────────────────────────────────────────────────
+
+static std::vector<std::string> makeIndex(const std::string &name)
+{
+    std::vector<std::string> v;
+    v.push_back(name);
+    return v;
+}
+
+static void createFile(const std::string &path, const std::string &content)
+{
+    std::ofstream f(path.c_str());
+    if (f.is_open())
+        f << content;
+}
+
+static void RUN(int n, const std::string &desc, int expected,
+                const std::string &method, const std::string &uri,
+                const ServerConfig &server, const std::string &body = "")
+{
+    HttpRequest req;
+    std::string m = method;
+    std::string u = uri;
+    req.addRequest("method", m);
+    req.addRequest("uri", u);
+    if (!body.empty())
+    {
+        std::string b = body;
+        req.addBody(b);
+    }
+    HttpResponse res = handleRequest(req, server);
+    std::cout << "[T" << n << "] " << desc << std::endl;
+    std::cout << "  Attendu : " << expected << std::endl;
+    std::cout << "  Recu    : " << res.serialize() << std::endl;
+    std::cout << std::endl;
+}
+
+static void setupTestFiles()
+{
+    createFile("./www/del/to_delete1.txt", "delete me 1");
+    createFile("./www/del/to_delete2.txt", "delete me 2");
+    createFile("./www/del/to_delete3.txt", "delete me 3");
+    createFile("./www/del/keep.txt", "do not delete");
+    createFile("./www/nested/page.html", "<html>nested</html>");
+    createFile("./www/files/readme.txt", "hello files");
+    createFile("./www/files/data.json", "{\"ok\":true}");
+    createFile("./www/dir_autoindex/sub.txt", "autoindex content");
+    createFile("./www/no_read/secret.txt", "forbidden content");
+    chmod("./www/no_read/secret.txt", 0000);
+    createFile("./www/dir_with_index/index.html", "<html>with index</html>");
+    createFile("./www/dir_both/index.html", "<html>both</html>");
+}
+
+// ─────────────────────────────────────────────────────────────────────────────
+// MAIN
+// ─────────────────────────────────────────────────────────────────────────────
 
 int main()
 {
-    // --- Construire la config ---
+    setupTestFiles();
+
     ServerConfig server;
+    server.setRoot("./www");
 
     LocationConfig locRoot;
     locRoot.setPath("/");
-    locRoot.setRoot("./www");
     locRoot.addMethod("GET");
+    locRoot.setIndex(makeIndex("index.html"));
     server.setLocations(locRoot);
 
-    LocationConfig locUpload;
-    locUpload.setPath("/upload");
-    locUpload.setRoot("./www/uploads");
-    locUpload.setUploadPath("./www/uploads");
-    locUpload.addMethod("POST");
-    server.setLocations(locUpload);
+    LocationConfig locAbout;
+    locAbout.setPath("/about");
+    locAbout.addMethod("GET");
+    server.setLocations(locAbout);
+
+    LocationConfig locNested;
+    locNested.setPath("/nested");
+    locNested.addMethod("GET");
+    locNested.setIndex(makeIndex("index.html"));
+    server.setLocations(locNested);
 
     LocationConfig locFiles;
     locFiles.setPath("/files");
-    locFiles.setRoot("./www/files");
+    locFiles.addMethod("GET");
     locFiles.addMethod("DELETE");
     server.setLocations(locFiles);
 
-    // --- Test 1 : GET valide ---
-    std::cout << "=== TEST 1 : GET ===" << std::endl;
-    {
-        HttpRequest req;
-        std::string method = "GET";
-        std::string uri = "/index.html";
-        req.addRequest("method", method);
-        req.addRequest("uri", uri);
-        HttpResponse res = handleRequest(req, server);
-        std::cout << res.serialize() << std::endl;
-    }
+    LocationConfig locDel;
+    locDel.setPath("/del");
+    locDel.addMethod("DELETE");
+    server.setLocations(locDel);
 
-    // --- Test 2 : POST valide ---
-    std::cout << "=== TEST 2 : POST ===" << std::endl;
-    {
-        HttpRequest req;
-        std::string method = "POST";
-        std::string uri = "/upload/test.txt";
-        std::string body = "Hello Upload!";
-        req.addRequest("method", method);
-        req.addRequest("uri", uri);
-        req.addBody(body);
-        HttpResponse res = handleRequest(req, server);
-        std::cout << res.serialize() << std::endl;
-    }
+    LocationConfig locUpload;
+    locUpload.setPath("/upload");
+    locUpload.addMethod("POST");
+    locUpload.setUploadPath("./www/uploads");
+    locUpload.setMaxBody(50);
+    locUpload.sethasmaxsize(true); // ← ajouter cette ligne
+    server.setLocations(locUpload);
 
-    // --- Test 3 : méthode non autorisée ---
-    std::cout << "=== TEST 3 : POST sur / (405) ===" << std::endl;
-    {
-        HttpRequest req;
-        std::string method = "POST";
-        std::string uri = "/index.html";
-        req.addRequest("method", method);
-        req.addRequest("uri", uri);
-        HttpResponse res = handleRequest(req, server);
-        std::cout << res.serialize() << std::endl;
-    }
+    LocationConfig locAutoindex;
+    locAutoindex.setPath("/dir_autoindex");
+    locAutoindex.addMethod("GET");
+    locAutoindex.setAutoindex(true);
+    server.setLocations(locAutoindex);
 
-    // --- Test 4 : location introuvable ---
-    std::cout << "=== TEST 4 : URI inconnue (404) ===" << std::endl;
-    {
-        HttpRequest req;
-        std::string method = "GET";
-        std::string uri = "/unknown/page";
-        req.addRequest("method", method);
-        req.addRequest("uri", uri);
-        HttpResponse res = handleRequest(req, server);
-        std::cout << res.serialize() << std::endl;
-    }
+    LocationConfig locNoIndex;
+    locNoIndex.setPath("/dir_no_index");
+    locNoIndex.addMethod("GET");
+    locNoIndex.setAutoindex(false);
+    server.setLocations(locNoIndex);
 
-    // --- Test 5 : méthode inconnue ---
-    std::cout << "=== TEST 5 : méthode PATCH (405) ===" << std::endl;
-    {
-        HttpRequest req;
-        std::string method = "PATCH";
-        std::string uri = "/index.html";
-        req.addRequest("method", method);
-        req.addRequest("uri", uri);
-        HttpResponse res = handleRequest(req, server);
-        std::cout << res.serialize() << std::endl;
-    }
+    LocationConfig locWithIndex;
+    locWithIndex.setPath("/dir_with_index");
+    locWithIndex.addMethod("GET");
+    locWithIndex.setIndex(makeIndex("index.html"));
+    locWithIndex.setAutoindex(false);
+    server.setLocations(locWithIndex);
 
+    LocationConfig locBoth;
+    locBoth.setPath("/dir_both");
+    locBoth.addMethod("GET");
+    locBoth.setIndex(makeIndex("index.html"));
+    locBoth.setAutoindex(true);
+    server.setLocations(locBoth);
+
+    LocationConfig locEmpty;
+    locEmpty.setPath("/empty_dir");
+    locEmpty.addMethod("GET");
+    locEmpty.setAutoindex(true);
+    server.setLocations(locEmpty);
+
+    LocationConfig locNoRead;
+    locNoRead.setPath("/no_read");
+    locNoRead.addMethod("GET");
+    server.setLocations(locNoRead);
+
+    // ════════════════════════════════════════════════════════════════
+    // CAT 1 : GET fichiers statiques (1-9)
+    // ════════════════════════════════════════════════════════════════
+    std::cout << "══════ GET STATIQUE ══════\n\n";
+
+    RUN(1, "GET /index.html → 200",
+        200, "GET", "/index.html", server);
+    RUN(2, "GET /nonexistent.html → 404",
+        404, "GET", "/nonexistent.html", server);
+    RUN(3, "GET /nested/page.html → 200",
+        200, "GET", "/nested/page.html", server);
+    RUN(4, "GET /files/readme.txt → 200 text/plain",
+        200, "GET", "/files/readme.txt", server);
+    RUN(5, "GET /files/data.json → 200 application/json",
+        200, "GET", "/files/data.json", server);
+    RUN(6, "GET /no_read/secret.txt → 403 (chmod 0000)",
+        403, "GET", "/no_read/secret.txt", server);
+    RUN(7, "GET /dir_autoindex/sub.txt → 200",
+        200, "GET", "/dir_autoindex/sub.txt", server);
+    RUN(8, "GET /del/keep.txt → 405 (GET interdit sur /del)",
+        405, "GET", "/del/keep.txt", server);
+    RUN(9, "GET /upload/anything → 405 (GET interdit sur /upload)",
+        405, "GET", "/upload/anything", server);
+
+    // ════════════════════════════════════════════════════════════════
+    // CAT 2 : GET répertoires (10-19)
+    // ════════════════════════════════════════════════════════════════
+    std::cout << "══════ GET RÉPERTOIRES ══════\n\n";
+
+    RUN(10, "GET / → 200 (index.html trouvé)",
+        200, "GET", "/", server);
+    RUN(11, "GET /dir_with_index/ → 200 (index.html trouvé)",
+        200, "GET", "/dir_with_index/", server);
+    RUN(12, "GET /dir_no_index/ → 403 (pas d'index, autoindex OFF)",
+        403, "GET", "/dir_no_index/", server);
+    RUN(13, "GET /dir_autoindex/ → 200 (listing HTML)",
+        200, "GET", "/dir_autoindex/", server);
+    RUN(14, "GET /empty_dir/ → 200 (listing vide)",
+        200, "GET", "/empty_dir/", server);
+    RUN(15, "GET /dir_both/ → 200 (index prioritaire sur autoindex)",
+        200, "GET", "/dir_both/", server);
+    RUN(16, "GET /dir_with_index → 301 (sans slash final)",
+        301, "GET", "/dir_with_index", server);
+    RUN(17, "GET /dir_autoindex → 301 (sans slash final)",
+        301, "GET", "/dir_autoindex", server);
+    RUN(18, "GET /empty_dir → 301 (sans slash final)",
+        301, "GET", "/empty_dir", server);
+    RUN(19, "GET /dir_no_index → 301 (sans slash final)",
+        301, "GET", "/dir_no_index", server);
+
+    // ════════════════════════════════════════════════════════════════
+    // CAT 3 : Sécurité traversal (20-26)
+    // ════════════════════════════════════════════════════════════════
+    std::cout << "══════ SÉCURITÉ ══════\n\n";
+
+    RUN(20, "GET /../etc/passwd → 403",
+        403, "GET", "/../etc/passwd", server);
+    RUN(21, "GET /files/../../../etc/passwd → 403",
+        403, "GET", "/files/../../../etc/passwd", server);
+    RUN(22, "GET /nested/../del/keep.txt → 403",
+        403, "GET", "/nested/../del/keep.txt", server);
+    RUN(23, "DELETE /../etc/passwd → 403",
+        403, "DELETE", "/../etc/passwd", server);
+    RUN(24, "POST /upload/../../evil.txt → 403 (traversal filename)",
+        403, "POST", "/upload/../../evil.txt", server, "body");
+    RUN(25, "GET /files/../../www/index.html → 403",
+        403, "GET", "/files/../../www/index.html", server);
+    RUN(26, "DELETE /del/../del/keep.txt → 403",
+        403, "DELETE", "/del/../del/keep.txt", server);
+
+    // ════════════════════════════════════════════════════════════════
+    // CAT 4 : POST upload (27-34)
+    // ════════════════════════════════════════════════════════════════
+    std::cout << "══════ POST ══════\n\n";
+
+    RUN(27, "POST /upload/file1.txt → 201",
+        201, "POST", "/upload/file1.txt", server, "Hello Upload!");
+    RUN(28, "POST /upload/file2.txt body vide → 201",
+        201, "POST", "/upload/file2.txt", server);
+    RUN(29, "POST /upload/big.txt body > 50 octets → 413",
+        413, "POST", "/upload/big.txt", server,
+        "This body is definitely more than fifty characters long!!!");
+    RUN(30, "POST /upload/ → 400 (nom de fichier vide)",
+        400, "POST", "/upload/", server, "data");
+    RUN(31, "POST /index.html → 405 (POST interdit sur /)",
+        405, "POST", "/index.html", server, "data");
+    RUN(32, "POST /files/test.txt → 405 (POST interdit sur /files)",
+        405, "POST", "/files/test.txt", server, "data");
+    RUN(33, "POST /del/test.txt → 405 (POST interdit sur /del)",
+        405, "POST", "/del/test.txt", server, "data");
+    RUN(34, "POST /upload/valid.bin → 201 (upload binaire)",
+        201, "POST", "/upload/valid.bin", server, "\x01\x02\x03");
+
+    // ════════════════════════════════════════════════════════════════
+    // CAT 5 : DELETE (35-41)
+    // ════════════════════════════════════════════════════════════════
+    std::cout << "══════ DELETE ══════\n\n";
+
+    RUN(35, "DELETE /del/to_delete1.txt → 204",
+        204, "DELETE", "/del/to_delete1.txt", server);
+    RUN(36, "DELETE /del/to_delete2.txt → 204",
+        204, "DELETE", "/del/to_delete2.txt", server);
+    RUN(37, "DELETE /del/to_delete3.txt → 204",
+        204, "DELETE", "/del/to_delete3.txt", server);
+    RUN(38, "DELETE /del/to_delete1.txt → 404 (déjà supprimé au T35)",
+        404, "DELETE", "/del/to_delete1.txt", server);
+    RUN(39, "DELETE /del/nonexistent.txt → 404",
+        404, "DELETE", "/del/nonexistent.txt", server);
+    RUN(40, "DELETE /del/ → 403 (tentative sur dossier)",
+        403, "DELETE", "/del/", server);
+    RUN(41, "DELETE /files/readme.txt → 204",
+        204, "DELETE", "/files/readme.txt", server);
+
+    // ════════════════════════════════════════════════════════════════
+    // CAT 6 : Location matching (42-46)
+    // ════════════════════════════════════════════════════════════════
+    std::cout << "══════ LOCATION MATCHING ══════\n\n";
+
+    RUN(42, "GET /dir_autoindex/sub.txt → 200 (/dir_autoindex > /)",
+        200, "GET", "/dir_autoindex/sub.txt", server);
+    RUN(43, "GET /uploadmore/test → 404 (/uploadmore != /upload, matche /)",
+        404, "GET", "/uploadmore/test", server);
+    RUN(44, "DELETE /files/data.json → 204 (DELETE autorisé sur /files)",
+        204, "DELETE", "/files/data.json", server);
+    RUN(45, "GET /about/ → 403 (dossier sans index ni autoindex)",
+        403, "GET", "/about/", server);
+    RUN(46, "GET /nested/ → 404 (index.html absent dans ./www/nested/)",
+        404, "GET", "/nested/", server);
+
+    // ════════════════════════════════════════════════════════════════
+    // CAT 7 : Edge cases (47-50)
+    // ════════════════════════════════════════════════════════════════
+    std::cout << "══════ EDGE CASES ══════\n\n";
+
+    RUN(47, "PATCH /index.html → 405",
+        405, "PATCH", "/index.html", server);
+    RUN(48, "HEAD /index.html → 405",
+        405, "HEAD", "/index.html", server);
+    RUN(49, "PUT /upload/file.txt → 405",
+        405, "PUT", "/upload/file.txt", server, "data");
+    RUN(50, "DELETE /del/to_delete2.txt → 404 (déjà supprimé au T36)",
+        404, "DELETE", "/del/to_delete2.txt", server);
+
+    chmod("./www/no_read/secret.txt", 0644);
     return 0;
 }
