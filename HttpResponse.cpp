74d2e39a8b6ff8c1df20e321db1b575d19780ce0
romukena <romukena@f1br7s7.paris.42.school>
2026-05-09 13:39:09 +0200

    correct addcode

--- HttpResponse.cpp
+++ HttpResponse.cpp
@@ -13,78 +13,99 @@
 #include "HttpResponse.hpp"
 #include <sstream>
 
-HttpResponse::HttpResponse() : _code(-1)
-{
+HttpResponse::HttpResponse() : _code(-1) {
 }
 
-HttpResponse::~HttpResponse()
-{
+HttpResponse::~HttpResponse() {
 }
 
-int HttpResponse::getCode() const
-{
+int HttpResponse::getCode() const {
 	return _code;
 }
 
-std::string HttpResponse::getVersion() const
-{
+std::string HttpResponse::getVersion() const {
 	return _version;
 }
 
-std::string HttpResponse::getMessage() const
-{
+std::string HttpResponse::getMessage() const {
 	return _message;
 }
 
-std::vector<unsigned char> HttpResponse::getBody() const
-{
+std::vector<unsigned char> HttpResponse::getBody() const {
 	return _body;
 }
 
-std::map<std::string, std::string> HttpResponse::getHeaders() const
-{
+std::map<std::string, std::string> HttpResponse::getHeaders() const {
 	return _headers;
 }
 
-void HttpResponse::addCode(int code)
-{
-	_code = code;
+static std::string codeReturn(int code) {
+	std::string e;
+	if (code == 200)
+		e = "OK";
+	else if (code == 201)
+		e = "Created";
+	else if (code == 204)
+		e = "No Content";
+	else if (code == 301)
+		e = "Moved Permanently";
+	else if (code == 400)
+		e = "Bad Request";
+	else if (code == 403)
+		e = "Forbidden";
+	else if (code == 404)
+		e = "Not Found";
+	else if (code == 405)
+		e = "Method Not Allowed";
+	else if (code == 413)
+		e = "Payload Too Large";
+	else if (code == 500)
+		e = "Internal Server Error";
+	else if (code == 501)
+		e = "Not Implemented";
+	else if (code == 502)
+		e = "Bad Gateway";
+	else if (code == 504)
+		e = "Gateway Timeout";
+	else 
+		e = "Unknown";
+	return e;
 }
-void HttpResponse::addVersion(std::string &e)
-{
+
+void HttpResponse::addCode(int code) {
+	_code = code;
+	_version = "HTTP/1.1";
+	_message = codeReturn(code);
+}
+
+void HttpResponse::addVersion(std::string &e) {
 	_version = e;
 }
 
-void HttpResponse::addMessage(std::string &e)
-{
+void HttpResponse::addMessage(std::string &e) {
 	_message = e;
 }
 
-void HttpResponse::addBodyResponse(std::string &e)
-{
+void HttpResponse::addBodyResponse(std::string &e) {
 	_body.insert(_body.end(), e.begin(), e.end());
 }
 
 void HttpResponse::addHeadersResponse(const std::string &key,
-									  const std::string &e)
-{
+									  const std::string &e) {
 	_headers.insert(std::pair<std::string, std::string>(key, e));
 }
 
-void HttpResponse::setBody(const std::vector<unsigned char> &body)
-{
+void HttpResponse::setBody(const std::vector<unsigned char> &body) {
 	_body = body;
 }
 
-std::string HttpResponse::serialize()
-{
+std::string HttpResponse::serialize() {
 	std::string final;
 	std::ostringstream oss;
 	oss << _code;
 	final = _version + " " + oss.str() + " " + _message + "\r\n";
 	std::map<std::string, std::string>::iterator it;
-	for (it = _headers.begin(); it != _headers.end(); ++it)
-	{
+	for (it = _headers.begin(); it != _headers.end(); ++it) {
 		std::string str;
 		str = it->first + ": " + it->second + "\r\n";
 		final += str;
